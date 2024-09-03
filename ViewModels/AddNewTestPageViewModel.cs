41727923dc28bf86ec8552813591ff8e2af93b24
Pelmen <alexi@PELMEN>
2024-09-03 10:52:52 +0300

    1

--- ViewModels/AddNewTestPageViewModel.cs
+++ ViewModels/AddNewTestPageViewModel.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TestingService.Models;
 
 namespace TestingService.ViewModels
@@ -19,6 +21,22 @@ namespace TestingService.ViewModels
             }
         }
 
+        public string TestTitle { get; set; }
+
+        public string TestDescription { get; set; }
+
+        private List<Questions> _questionsList;
+        public List<Questions> QuestionsList
+        {
+            get => _questionsList;
+            set
+            {
+                _questionsList = value;
+                NotifyPropertyChanged("QuestionsList");
+            }
+        }
+
+
         private RelayCommand _addNewQuestion;
         public RelayCommand AddNewQuestion
         {
@@ -31,10 +49,57 @@ namespace TestingService.ViewModels
             }
         }
 
+        private RelayCommand _saveTest;
+        public RelayCommand SaveTest
+        {
+            get
+            {
+                return _saveTest ?? new RelayCommand(obj =>
+                {
+                    UpdateData();
+                });
+            }
+        }
+
+        private Tests _currentTest;
 
         public AddNewTestPageViewModel()
         {
+            _currentTest = new Tests();
+            QuestionsList = new List<Questions>();
             DatabaseConnection.connection = new DatabaseEntities();
+        }
+
+        private void UpdateData()
+        {
+            if (CheckingData())
+            {
+                _currentTest.TestTitle = TestTitle;
+                try
+                {
+                    _currentTest.TestDescription = TestDescription;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Неопознанная ошибка");
+                    Console.WriteLine(ex.Message);
+                }
+                MessageBox.Show($"{_currentTest.TestId} {_currentTest.TestTitle} {_currentTest.TestDescription}");
+            }
+            else
+            {
+                MessageBox.Show("Данные не сохранены");
+            }
+        }
+
+        private bool CheckingData()
+        {
+            if ((TestTitle == null) /*|| (QuestionsList.Count <= 0)*/)
+            {
+                MessageBox.Show("Не все поля заполнены!");
+                return false;
+            }
+            return true;
         }
 
         private void NewQuestion()
---
+++ ViewModels/QuestionEditorViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingService.ViewModels
+{
+    public class QuestionEditorViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+    }
+}
